283cc0db76f9937e4e14fe0ba84f62b6a4d813f6
miarothman <rothman.mia@gmail.com>
2022-08-19 13:59:16 -0400

    Make function that plays a single round of Rock, Paper, Scissors.

--- rps.js
+++ rps.js
@@ -17,4 +17,41 @@ function getComputerChoice() {
    return randomGameOption;
 }
 
-console.log(getComputerChoice());
+/*
+PSEUDOCODE FOR SECOND STEP:
+1. Make function that plays a single round of Rock Paper Scissors
+2. Function takes 2 parameters - playerSelection and computerSelection
+3. Return string that declares winner of the round: "You Lose! Paper beats Rock"
+4. Make your function’s playerSelection parameter case-insensitive so users can input rock, ROCK, RocK or any other variations
+5. Return the results of this function call, not console.log() them.
+
+How to make case-insensitive?
+convert input to either all lower or uppercase
+The toUpperCase() method converts a string to uppercase letters.
+let text = "Hello World!";
+let result = text.toUpperCase();
+*/
+
+function playRound(playerSelection, computerSelection){
+if (playerSelection === computerSelection){
+    console.log("It's a tie!");
+} else if (playerSelection === gameOptions[0] && computerSelection === gameOptions[1]){
+    console.log("You Lose! Paper beats Rock!");
+} else if (playerSelection === gameOptions[0] && computerSelection === gameOptions[2]){
+    console.log("You Win! Rock beats Scissors!");
+} else if (playerSelection === gameOptions[1] && computerSelection === gameOptions[0]){
+    console.log("You Win! Paper beats Rock!");
+} else if (playerSelection === gameOptions[1] && computerSelection === gameOptions[2]){
+    console.log("You Lose! Scissors beats Paper!");
+} else if (playerSelection === gameOptions[2] && computerSelection === gameOptions[0]){
+    console.log("You Lose! Rock beats Scissors!");
+} else if (playerSelection === gameOptions[2] && computerSelection === gameOptions[1]){
+    console.log("You Win! Scissors beats Paper");
+}
+}
+const playerSelection = gameOptions[0];
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
+
